1cacde73157a5f84a3aff3301a36e44419f8bf69
hiendh <hiendaoit@gmail.com>
2023-09-06 10:05:23 +0700

    init

---
+++ portal/src/webparts/menu/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IMenuWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'MenuWebPartStrings' {
+  const strings: IMenuWebPartStrings;
+  export = strings;
+}
